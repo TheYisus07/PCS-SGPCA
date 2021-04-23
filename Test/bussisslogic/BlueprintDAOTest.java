e7dcbac785680d8e8aeeeaf235919bb00a312798
José Daniel Camarillo Villa <78821964+JDaniCV@users.noreply.github.com>
2021-04-22 21:50:28 -0500

    DAO de Blueprint en conjunto de sus pruebas terminadas

---
+++ Test/bussisslogic/BlueprintDAOTest.java
@@ -0,0 +1,60 @@
+package bussisslogic;
+
+import businesslogic.BlueprintDAO;
+import domain.Blueprint;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * 
+ * @author daniCV
+ */
+
+public class BlueprintDAOTest {
+    
+    @Test
+    public void testAddBlueprintSuccessfully() {
+        Date startDate = new Date((2023-1900), (4-1), 12);
+        Blueprint blueprint = new Blueprint("Teoría de los microservicios", startDate, "LGAC1, LGCA2", "Asignada", "Tesina", "Carlos Edson Romero", "Proyecto de investigación dedicado a la intervención...", "Jorge Octavio Ocharán Hernández", "Machine Learning");
+        BlueprintDAO blueprintDAO = new BlueprintDAO();
+        int successfulSave = blueprintDAO.addBlueprint(blueprint);
+        Assert.assertEquals(1, successfulSave);
+    }
+    /*
+    @Test
+    public void testUpdateProjectSuccessfullu() {    
+        Date startDate = new Date((2024-1900), (10-1), 1);
+        BlueprintDAO blueprint = new BlueprintDAO();
+        Blueprint updatedBlueprint = new Blueprint("Teoría de los microservicios", startDate, "LGAC1, LGCA2", "Asignada", "Tesina", "José Daniel Camarillo Villa", "Proyecto de investigación dedicado a la intervención...", "Jorge Octavio Ocharán Hernández", "Machine Learning");
+        int updatedSuccessful = blueprint.modifyBlueprint(updatedBlueprint);
+        Assert.assertEquals(1, updatedSuccessful);
+    }
+    
+    @Test
+    public void testConsultBlueprintByTitle() {
+        BlueprintDAO blueprint = new BlueprintDAO();
+        Blueprint blueprintResult;
+        String blueprintTitleExpected = "Teoria de los microservicios";
+        blueprintResult = blueprint.consultBlueprintByTitle(blueprintTitleExpected);
+        String blueprintTitleActual = blueprintResult.getTitle();
+        Assert.assertEquals("Prueba obtener otro Anteprojecto", blueprintTitleExpected, blueprintTitleActual);
+    }
+    
+    public void testConsultBlueprintByDate() {
+        BlueprintDAO blueprint = new BlueprintDAO();
+        Blueprint blueprintResult;
+        String blueprintDateExpected = (new SimpleDateFormat("yyyy-MM-dd").format("2024-09-01"));
+        blueprintResult = blueprint.consultBlueprintByDate(blueprintDateExpected);
+        String blueprintDateActual = (new SimpleDateFormat("yyyy-MM-dd").format(blueprintResult.getStartDate()));
+        Assert.assertEquals("Prueba obtener otro Anteprojecto", blueprintDateExpected, blueprintDateActual);
+    }
+    
+    @Test
+    public void testDeleteBlueprintByTitle() {
+        BlueprintDAO blueprint = new BlueprintDAO();
+        int deleteResult = blueprint.deleteBlueprintByTitle("Teoria de los microservicios");
+        Assert.assertEquals(1, deleteResult);
+    }*/
+}
